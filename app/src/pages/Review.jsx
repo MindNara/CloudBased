6c609e7157c928a42cb422637bc94887ba1e4b5b
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2024-03-02 17:53:21 +0700

    add filter subject

--- app/src/pages/Review.jsx
+++ app/src/pages/Review.jsx
@@ -6,7 +6,10 @@ import axios from 'axios';
 
 function Review() {
   const [textSearch, setTextSearch] = useState('');
+  const [typeSearch, setTypeSearch] = useState('ทั้งหมด');
   const [subjects, setSubjects] = useState([]);
+  const [subjectItem, setSubjectItem] =useState([]);
+
   useEffect(() => {
     const fetchDataSubject = async () => {
       try {
@@ -21,37 +24,49 @@ function Review() {
     fetchDataSubject();
   }, []);
 
+  const filterSubject = () => {
+    console.log(typeSearch);
+    console.log(textSearch);
+    const filteredList = subjects.filter(item =>
+      (item.subject_id.S.toLowerCase().includes(textSearch.toLowerCase()) ||
+        item.subject_name_th.S.toLowerCase().includes(textSearch.toLowerCase()) ||
+        item.subject_name_en.S.toLowerCase().includes(textSearch.toLowerCase())) &&
+        (item.type.S == typeSearch ||  typeSearch == "ทั้งหมด")
+    );
+    console.log(filteredList);
+    setSubjectItem(filteredList);
+    console.log("eiei " + subjectItem.length);
+  }
+
+  const clearTextSearch = () => {
+    filterSubject();
+    setTextSearch('');
+  }
+
   return (
     <>
       <div className='w-full h-auto flex'>
         <div className='w-full pr-5'>
           <div className='flex flex-row gap-3'>
             <h1 className='text-[26px] font-medium text-[#151C38]'>รายวิชาเรียน</h1>
-            <select className="text-white bg-[#151C38] rounded-lg text-lg px-2" name='selectYear'>
-              <option value="year" disabled selected>ชั้นปี</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
-            <select className="text-white bg-[#151C38] rounded-lg text-lg px-2" name='selectType'>
-              <option value="type" disabled selected>ประเภทวิชา</option>
-              <option value="type1">วิชาบังคับ</option>
-              <option value="type2">วิชาเสรีทั่วไป</option>
-              <option value="Type3">วิชาเสรีคณะ</option>
-              <option value="Type4">ทั้งหมด</option>
+            <select className="text-white bg-[#151C38] rounded-lg text-lg px-2" name='selectType' defaultValue={typeSearch} onChange={(e) => setTypeSearch(e.target.value)} onClick={filterSubject}>
+              <option value="วิชาบังคับ">วิชาบังคับ</option>
+              <option value="เสรีทั่วไป">วิชาเสรีทั่วไป</option>
+              <option value="เสรีคณะ">วิชาเสรีคณะ</option>
+              <option value="ทั้งหมด">ทั้งหมด</option>
             </select>
           </div>
           <div className='inputSearch flex flex-row mt-4 gap-3 drop-shadow-sm	'>
             <img width="35" height="35" src='https://img.icons8.com/fluency-systems-filled/48/c0c0c0/search.png' className='icon mt-1 ml-2'></img>
-            <input type='text' name='email' placeholder='Type to search ...' className='w-full h-[45px] font-light' value={textSearch} onChange={(e) => setTextSearch(e.target.value)}></input>
-            <button className='bg-[#FFFFFF] w-[60px] rounded-xl border-[1px] border-[#D9D9D9] drop-shadow-sm' onClick={() => setTextSearch('')}>
+            <input type='text' name='email' placeholder='Type to search ...' className='w-full h-[45px] font-light' value={textSearch} onChange={(e) => setTextSearch(e.target.value)} onKeyDown={(e) => { 
+              if (e.key === 'Enter') { 
+                filterSubject();
+            }}}></input>
+            <button className='bg-[#FFFFFF] w-[60px] rounded-xl border-[1px] border-[#D9D9D9] drop-shadow-sm' onClick={clearTextSearch}>
               <img width="20" height="20" src='https://img.icons8.com/material-rounded/24/737373/delete-sign.png' className='icon top-3 ml-4'></img>
             </button>
           </div>
-          {subjects && (
-            <CardSubject item={subjects} />
-          )}
+          {textSearch != '' || typeSearch != 'ทั้งหมด' ? (<CardSubject item={subjectItem} />) : (<CardSubject item={subjects} />)}
         </div>
       </div>
     </>
